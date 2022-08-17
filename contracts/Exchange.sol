d7867fd12da2822805e36ec9b3bd83105478ef5a
ahmedzaheerSys <ahmed.zaheer@systemsltd.com>
2022-08-17 10:18:38 +0500

    create exchange

---
+++ contracts/Exchange.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Import this file to use console.log
+import "hardhat/console.sol";
+
+contract Exchange {
+    address public feeAccount;
+    uint256 public feePercent;
+
+    constructor(address _feeAccount , uint256 _feePercent) {
+        feeAccount = _feeAccount;
+        feePercent = _feePercent;
+    }
+}
