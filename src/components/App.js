91ae4cc7b5b12717c64244c421bcd345795e91bc
ahmedzaheerSys <ahmed.zaheer@systemsltd.com>
2022-08-19 20:14:27 +0500

    navbar components and network and account changing handled

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,8 @@ import {
   loadExchange
 } from '../store/interactions';
 
+import Navbar from './Navbar'
+
 function App() {
   const dispatch = useDispatch()
 
@@ -20,12 +22,21 @@ function App() {
     // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
 
-    // Fetch current account & balance from Metamask
-    await loadAccount(provider, dispatch)
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
+    })
+
+    // Fetch current account & balance from Metamask when changed
+    window.ethereum.on('accountsChanged', () => {
+      loadAccount(provider, dispatch)
+    })
 
     // Load token smart contracts
     const DApp = config[chainId].DApp
-    const mETH = config[chainId].mETH
+    const mETH = config[chainId].mETH;
+    console.log("----DApp.---",DApp);
+    console.log("----mETH.---",mETH);
     await loadTokens(provider, [DApp.address, mETH.address], dispatch)
 
     // Load exchange smart contract
@@ -40,7 +51,7 @@ function App() {
   return (
     <div>
 
-      {/* Navbar */}
+      <Navbar />
 
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
